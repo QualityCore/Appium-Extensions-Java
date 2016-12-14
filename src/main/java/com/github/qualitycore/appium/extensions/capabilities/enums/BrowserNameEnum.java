8d4220af0bac8fbe52671335ce566c9aadfb194f
Leonardo Carmona <leocarmona_@outlook.com>
2016-12-14 12:26:05 -0200

    Implementação do enum dos browsers mobile.

---
+++ src/main/java/com/github/qualitycore/appium/extensions/capabilities/enums/BrowserNameEnum.java
@@ -0,0 +1,26 @@
+package com.github.qualitycore.appium.extensions.capabilities.enums;
+
+import io.appium.java_client.remote.MobileBrowserType;
+
+public enum BrowserNameEnum {
+
+	SAFARI(MobileBrowserType.SAFARI),
+	BROWSER(MobileBrowserType.BROWSER),
+	CHROMIUM(MobileBrowserType.CHROMIUM),
+	CHROME(MobileBrowserType.CHROME);
+
+	private final String value;
+
+	private BrowserNameEnum(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	@Override
+	public String toString() {
+		return this.getValue();
+	}
+}
